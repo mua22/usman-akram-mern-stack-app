9ae4ac51d8367c047a55fe027a8c12cf74c9fdc1
Usman Akram <usman.akram@gmail.com>
2021-06-12 16:49:55 +0500

    Update index.js

--- cron-jobs/index.js
+++ cron-jobs/index.js
@@ -2,6 +2,7 @@ var cron = require("node-cron");
 var clearProducts = require("./clearProducts");
 function startCronJobs() {
   console.log("Starting Cron Jobs");
+  clearProducts();
   cron.schedule("*/30 * * * *", () => {
     clearProducts();
   });
